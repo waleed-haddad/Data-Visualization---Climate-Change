5aaff1c9f79fd95075f5132b1c4851dfca32f689
Jay Patel <jaypatel@MacBook-Pro-2.local>
2022-12-04 11:31:19 -0500

    [DEV 2.2] Updated NightModeDisplay and ColourBlindDisplay

---
+++ DefaultView.java
@@ -0,0 +1,7 @@
+package views;
+
+import org.controlsfx.control.WorldMapView;
+
+public interface DefaultView {
+    public void updateDisplay();
+}
--- NightModeDisplay.java
+++ NightModeDisplay.java
@@ -1,17 +1,19 @@
 package views;
-import java.util.*;
 
 import javafx.scene.paint.Color;
 import org.controlsfx.control.WorldMapView;
 
-public class ColourBlindDisplay implements DefaultView {
+public class NightModeDisplay implements DefaultView{
     private final Color colour;
     private final WorldMapView.CountryView view;
-    public ColourBlindDisplay(WorldMapView.CountryView view) {
+
+    public NightModeDisplay(WorldMapView.CountryView view) {
         this.view = view;
-        this.colour = Color.rgb(0, 114, 178);
+        this.colour = Color.rgb(0, 0, 51);
     }
+
     public void updateDisplay() {
         this.view.setFill(this.colour);
     }
 }
+
