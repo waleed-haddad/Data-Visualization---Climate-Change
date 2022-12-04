c20f945626e2f74ff9d73c1871de101f593786db
Jay Patel <jaypatel@MacBook-Pro-2.local>
2022-12-04 14:45:36 -0500

    [DEV 2.3] making views compatiable

---
+++ NightModeDisplay.java
@@ -0,0 +1,17 @@
+import javafx.scene.paint.Color;
+import org.controlsfx.control.WorldMapView;
+
+public class NightModeDisplay implements DefaultView{
+    private final Color colour;
+    private final WorldMapView.CountryView view;
+
+    public NightModeDisplay(WorldMapView.CountryView view) {
+        this.view = view;
+        this.colour = Color.rgb(0, 0, 51);
+    }
+
+    public void updateDisplay() {
+        this.view.setFill(this.colour);
+    }
+}
+
