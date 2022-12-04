03ea6273d7b71b4ebfc2f1876737a629ad6c0595
Jay Patel <jaypatel@MacBook-Pro-2.local>
2022-12-04 12:04:40 -0500

    [DEV 2.2] deleted conflicting files in order to merge

--- EventListener.java
+++
@@ -1,8 +0,0 @@
-public interface EventListener {
-
-    /**
-     * Update the Event accordingly depending on the state
-     * @param state : boolean value
-     */
-    public void update(boolean state);
-}
